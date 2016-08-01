2f57dab2f38e4efc032767d205e469a9359f0778
Ben Tyler <benjamin.tyler@gmail.com>
2016-08-01 22:01:30 +0200

    Ditch gratuitous concurrency on signal handler
    dgryski pointed out that the combination of sync.WaitGroup and goroutine is the
    same as having a plain blocking channel read.

--- main.go
+++ main.go
@@ -6,26 +6,21 @@ import (
 	"github.com/kanatohodets/go-match/matchbot"
 	"os"
 	"os/signal"
-	"sync"
 )
 
 func main() {
 	log.SetLevel(log.InfoLevel)
-	var wg sync.WaitGroup
-	wg.Add(1)
+
 	matchbot := matchbot.New()
 	go matchbot.Start("localhost:8200", "FooUser", "foobar", "blorg.json")
 
 	// gracefully exit on SIGINT
 	// (mostly, make sure the server is told to clean up queues that this bot hosted)
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		<-c
-		fmt.Printf("exiting ...\n")
-		matchbot.Shutdown()
-		os.Exit(1)
-	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
 
-	wg.Wait()
+	fmt.Printf("exiting ...\n")
+	matchbot.Shutdown()
+	os.Exit(1)
 }
