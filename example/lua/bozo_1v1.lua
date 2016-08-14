1423cb62cff33e65d6dd0a567b1178c6db0d1490
Ben Tyler <benjamin.tyler@gmail.com>
2016-08-14 16:50:37 +0200

    lua example: match in 'Update' instead of 'PlayerJoined'

--- example/lua/bozo_1v1.lua
+++ example/lua/bozo_1v1.lua
@@ -1,47 +1,12 @@
 local players = {}
 
-local function playerList()
-	local list = {}
-	for playerName, playerData in pairs(players) do
-		table.insert(list, playerData)
-	end
-	return list
-end
-
 -- TODO: richer player data structure. perhaps store 'players' outside the lua?
 function queue.PlayerJoined(playerName)
-	print("I now see a player to match! ", playerName, " ", queue.GetTitle())
 	players[playerName] = {
 		name = playerName,
 		skillLevel = 9001
 	}
 
-	local playerList = playerList()
-	if #playerList == 2 then
-		local maps = queue.GetMapList()
-		local games = queue.GetGameList()
-
-		queue.NewMatch({
-			map = maps[1],
-			game = games[1],
-			engineVersion = "99",
-			players = {
-				{
-					name = playerList[1].name,
-					ally = 0,
-					team = 0,
-				},
-				{
-					name = playerList[2].name,
-					ally = 1,
-					team = 1,
-				}
-			}
-		})
-
-		players[playerList[1]] = nil
-		players[playerList[2]] = nil
-	end
 end
 
 function queue.PlayerLeft(playerName)
@@ -50,8 +15,35 @@ function queue.PlayerLeft(playerName)
 end
 
 function queue.Update(n)
-	if n % 30 == 0 then
-		print(queue.GetTitle(), " has been alive for ", n, " seconds")
+	local title = queue.GetTitle()
+	if n % 5 == 0 then
+		local playerList = queue.GetPlayerList()
+		if #playerList == 2 then
+			print(queue.GetTitle(), " omg two players to match ", n)
+			local maps = queue.GetMapList()
+			local games = queue.GetGameList()
+
+			queue.NewMatch({
+				map = maps[1],
+				game = games[1],
+				engineVersion = "99",
+				players = {
+					{
+						name = playerList[1],
+						ally = 0,
+						team = 0,
+					},
+					{
+						name = playerList[2],
+						ally = 1,
+						team = 1,
+					}
+				}
+			})
+
+			players[playerList[1]] = nil
+			players[playerList[2]] = nil
+		end
 	end
 end
 
